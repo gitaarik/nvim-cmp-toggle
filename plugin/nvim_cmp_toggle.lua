ff878a7db07f40a1acfaaf3e7de783767c6c8442
Rik <gitaarik@posteo.net>
2023-12-12 01:44:35 +0100

    initial commit

---
+++ plugin/nvim_cmp_toggle.lua
@@ -0,0 +1,16 @@
+local function toggle_autocomplete()
+  local cmp = require('cmp')
+  local current_setting = cmp.get_config().completion.autocomplete
+  if current_setting and #current_setting > 0 then
+    cmp.setup({ completion = { autocomplete = false } })
+    print('Autocomplete disabled')
+  else
+    cmp.setup({ completion = { autocomplete = { cmp.TriggerEvent.TextChanged } } })
+    print('Autocomplete enabled')
+  end
+end
+
+vim.api.nvim_create_user_command('NvimCmpToggle', toggle_autocomplete, {})
+
+-- Set a keymap like this for example:
+-- vim.api.nvim_set_keymap('n', '<Leader>a', ':NvimCmpToggle<CR>', { noremap = true, silent = true })
